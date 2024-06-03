73003b86d344e7f65a2163e9163b073cf1040ff2
brillande <jelland90@gmail.com>
2024-06-03 17:40:44 +0200

    finalizado

--- push_swap.c
+++ push_swap.c
@@ -6,11 +6,23 @@
 /*   By: emedina- <emedina-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/28 13:25:07 by emedina-          #+#    #+#             */
-/*   Updated: 2024/03/06 13:11:50 by emedina-         ###   ########.fr       */
+/*   Updated: 2024/06/03 17:38:36 by emedina-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "./inc/push_swap.h"
+
+void	freematrix(char	**ptr)
+{
+	int	i;
+
+	i = -1;
+	while (ptr[++i])
+	{
+		free(ptr[i]);
+	}
+	free(ptr);
+}
 
 int	main(int argc, char **argv)
 {
@@ -19,12 +31,16 @@ int	main(int argc, char **argv)
 
 	a = NULL;
 	b = NULL;
-	if (argc == 1 || (argc == 2 && !argv[1][0]) || (argc == 2
-			&& argv[1][0] == ' '))
+	if (argc == 1 || (argc == 2 && !argv[1][0]))
 		return (1);
 	else if (argc == 2)
+	{
 		argv = ft_split(argv[1], ' ');
-	init_stack_a(&a, argv + 1);
+		init_stack_a(&a, argv);
+		freematrix(argv);
+	}
+	else
+		init_stack_a (&a, argv + 1);
 	if (!is_sorted(a))
 	{
 		if (ft_lstsize(a) == 2)
@@ -35,6 +51,4 @@ int	main(int argc, char **argv)
 			sort_list(&a, &b);
 	}
 	free_list(&a);
-	free_errors(&a);
-	return (0);
 }
